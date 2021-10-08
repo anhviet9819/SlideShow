05d7c2fec23bc322e21889ec0ecafd961ebdb65f
unknown <anhviet9819@gmail.com>
2021-10-08 17:25:51 +0700

    Published github pages

---
+++ script.js
@@ -0,0 +1,34 @@
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+//Dot controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    // console.log(n); 
+}
+
+setInterval(function(){
+    showSlides(slideIndex >= slides.length ? slideIndex = 1 : slideIndex++);
+},3000);
